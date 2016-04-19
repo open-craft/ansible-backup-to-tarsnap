d7b1befabab574de4b400fa2d152c2f570377dcd
Jacek Bzdak <jacek@opencraft.com>
2016-04-20 00:29:01 +0200

    Cleaned logic a bit

--- templates/backup.sh
+++ templates/backup.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash
 
-timestamp = $(date -Isecond)
+# Logic is as follows:
 
-{% if TARSNAP_PRE_BACKUP_SCRIPT_REMOTE_LOCATION %}
-{{ TARSNAP_PRE_BACKUP_SCRIPT_REMOTE_LOCATION }} || exit 1
+# 1. If pre-script fails exit immediately
+# 2. Proceed if backup
+# 3. Irrespective whether backup succeeds execute post-script.
+
+timestamp=$(date -Isecond)
+
+{% if TARSNAP_BACKUP_PRE_SCRIPT %}
+{{ TARSNAP_BACKUP_PRE_SCRIPT }} || exit 1
 {% endif %}
 
 /usr/local/bin/tarsnap -c --keyfile "{{ TARSNAP_KEY_REMOTE_LOCATION }}" \
 --cachedir "{{ TARSNAP_CACHE }}"  -f "{{ TARSNAP_ARCHIVE_NAME }}-${timestamp}" \
 "{{ TARSNAP_BACKUP_FOLDERS }}" {% if TARSNAP_BACKUP_SNITCH %} && curl {{ TARSNAP_BACKUP_SNITCH }} {% endif %}
 
-{{ TARSNAP_POST_BACKUP_SCRIPT_REMOTE_LOCATION }}
+{% if TARSNAP_BACKUP_POST_SCRIPT %}{{ TARSNAP_BACKUP_POST_SCRIPT }}{% endif %}
