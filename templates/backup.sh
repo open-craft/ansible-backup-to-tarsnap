4c384a9b0901a9278ad2745018f84b5463a17cdf
Jacek Bzdak <jacek@opencraft.com>
2016-04-19 21:40:27 +0200

    New version of tarsnap role

---
+++ templates/backup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+timestamp = $(date -Isecond)
+
+{% if TARSNAP_PRE_BACKUP_SCRIPT_REMOTE_LOCATION %}
+    {{ TARSNAP_PRE_BACKUP_SCRIPT_REMOTE_LOCATION }} || exit 1
+{% endif %}
+
+/usr/local/bin/tarsnap -c --keyfile "{{ TARSNAP_KEY_REMOTE_LOCATION }}" \
+--cachedir "{{ TARSNAP_CACHE }}"  -f "{{ DALITE_LOG_TARSNAP_ARCHIVE }}-${timestamp}" \
+"{{ TARSNAP_BACKUP_FOLDERS }}" \
+{% if TARSNAP_BACKUP_SNITCH %} && curl {{ TARSNAP_BACKUP_SNITCH }} {% endif %}
+
+{{ TARSNAP_POST_BACKUP_SCRIPT_REMOTE_LOCATION }}
